3e4f949be66062ab27ddb5c5bbb8ebb442462fdb
abul <shan007@gmail.com>
2016-01-22 22:39:22 -0500

    Week3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculate inverse of inputted matrix
 
-## Write a short comment describing this function
+## Cache the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  
+  set <- function(x) {
+    x <<- x
+    m <<- NULL
+  }
+  get <- function()x
+  set_inv_mat <- function(inv_mat) m <<- inv_mat
+  get_inv_mat <- function() m 
 
+
+list(set = set, get = get,
+     set_inv_mat = set_inv_mat,
+     get_inv_mat = get_inv_mat)
 }
 
 
-## Write a short comment describing this function
+
+
+## Calculate the inverse of matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$get_inv_mat()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$set_inv_mat(m)
+  m
+  
 }
